07cc8878c7e8b3976fdfda61b53edf08551d872f
marco.spasiano <marco.spasiano@cnr.it>
2024-12-04 09:01:03 +0100

    change workflow definition

--- create_and_start.sh
+++ create_and_start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-curl -X POST http://as4dock.test.si.cnr.it:9080/conductor-server/api/metadata/workflow -H 'Content-type:application/json' --data @crawler_amministrazione_trasparente.json 
-curl -X POST http://as4dock.test.si.cnr.it:9080/conductor-server/api/metadata/workflow -H 'Content-type:application/json' --data @crawler_result_failed.json 
-curl -X POST http://as4dock.test.si.cnr.it:9080/conductor-server/api/metadata/workflow -H 'Content-type:application/json' --data @rule_workflow.json 
-curl -X POST http://as4dock.test.si.cnr.it:9080/conductor-server/api/metadata/workflow -H 'Content-type:application/json' --data @rule_detail_workflow.json 
-curl -X POST http://as4dock.test.si.cnr.it:9080/conductor-server/api/metadata/workflow -H 'Content-type:application/json' --data @rule_detail_child_workflow.json
+curl -X POST http://localhost:8080/api/metadata/workflow -H 'Content-type:application/json' --data @crawler_amministrazione_trasparente.json 
+curl -X POST http://localhost:8080/api/metadata/workflow -H 'Content-type:application/json' --data @crawler_result_failed.json 
+curl -X POST http://localhost:8080/api/metadata/workflow -H 'Content-type:application/json' --data @rule_workflow.json 
+curl -X POST http://localhost:8080/api/metadata/workflow -H 'Content-type:application/json' --data @rule_detail_workflow.json 
+curl -X POST http://localhost:8080/api/metadata/workflow -H 'Content-type:application/json' --data @rule_detail_child_workflow.json
 
-curl -X POST http://as4dock.test.si.cnr.it:9080/conductor-server/api/workflow -H 'Content-type:application/json' --data @start_amm_trasparente.json
+curl -X POST http://localhost:8080/api/workflow -H 'Content-type:application/json' --data @start_amm_trasparente.json
