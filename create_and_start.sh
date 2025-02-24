51b9b84a4739ea3851207f6af7450581abff23da
marco.spasiano <marco.spasiano@cnr.it>
2025-02-24 16:28:03 +0100

    fix workflow and add image

--- create_and_start.sh
+++ create_and_start.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 
-curl -X POST http://localhost:8080/api/metadata/workflow -H 'Content-type:application/json' --data @crawler_amministrazione_trasparente.json 
-curl -X POST http://localhost:8080/api/metadata/workflow -H 'Content-type:application/json' --data @crawler_result_failed.json 
-curl -X POST http://localhost:8080/api/metadata/workflow -H 'Content-type:application/json' --data @rule_workflow.json 
-curl -X POST http://localhost:8080/api/metadata/workflow -H 'Content-type:application/json' --data @rule_detail_workflow.json 
-curl -X POST http://localhost:8080/api/metadata/workflow -H 'Content-type:application/json' --data @rule_detail_child_workflow.json
+if [ -z "$1" ]
+  then
+    echo "Paramete missing, please enter the URL to get the TOKEN"
+    exit 1
+fi
 
-curl -X POST http://localhost:8080/api/workflow -H 'Content-type:application/json' --data @start_amm_trasparente.json
+if [ -z "$2" ]
+  then
+    echo "Parameter missing, please enter the client id to get the TOKEN"
+    exit 1
+fi
+
+if [ -z "$3" ]
+  then
+    echo "Parameter missing, please enter the client secret to get the TOKEN"
+    exit 1
+fi
+
+
+if [ -z "$4" ]
+  then
+    echo "Parameter missing, please enter the BASE URL to conductor"
+    exit 1
+fi
+
+TOKEN=$(curl $1/protocol/openid-connect/token \
+	-H 'accept: application/json, text/plain, */*' \
+	--data 'grant_type=client_credentials&client_id='$2'&client_secret='$3''| jq -r '.access_token')
+
+printf "Try to create crawler_amministrazione_trasparente \n"
+( echo '['; cat crawler_amministrazione_trasparente.json ; echo ']') | curl -X PUT $4/api/metadata/workflow -H "Authorization: Bearer $TOKEN" -H 'Content-type:application/json' --data @- 
+printf "\nTry to create crawler_result_failed\n"
+( echo '['; cat crawler_result_failed.json ; echo ']') | curl -X PUT $4/api/metadata/workflow -H "Authorization: Bearer $TOKEN" -H 'Content-type:application/json' --data @- 
+printf "\nTry to create rule_workflow\n"
+( echo '['; cat rule_workflow.json ; echo ']') | curl -X PUT $4/api/metadata/workflow -H "Authorization: Bearer $TOKEN" -H 'Content-type:application/json' --data @- 
+printf "\nTry to create rule_detail_workflow\n"
+( echo '['; cat rule_detail_workflow.json ; echo ']') | curl -X PUT $4/api/metadata/workflow -H "Authorization: Bearer $TOKEN" -H 'Content-type:application/json' --data @-
+printf "\nTry to create rule_detail_child_workflow\n"
+( echo '['; cat rule_detail_child_workflow.json ; echo ']') | curl -X PUT $4/api/metadata/workflow -H "Authorization: Bearer $TOKEN" -H 'Content-type:application/json' --data @-
+printf "\nTry to create rule_detail_html_source_workflow\n"
+( echo '['; cat rule_detail_html_source_workflow.json ; echo ']') | curl -X PUT $4/api/metadata/workflow -H "Authorization: Bearer $TOKEN" -H 'Content-type:application/json' --data @-
+printf "\nTry to START workflow\n"
+curl -X POST $4/api/workflow -H "Authorization: Bearer $TOKEN" -H 'Content-type:application/json' --data @start_amm_trasparente.json
+printf "\n"
